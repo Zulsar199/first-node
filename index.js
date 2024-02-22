e9748589c159c9a42eae73b336cd108d65f0e067
Zulsar199 <zulsar199@gmail.com>
2024-02-22 12:42:34 +0700

    app get, post, delete

--- index.js
+++ index.js
@@ -11,34 +11,42 @@ const app = express();
 //     response.json({name: "zula"})
 // })
 
-// app.listen(port, ()=>{
-//     console.log(`ene port deer server  aslaa http://localhost:${port}`);
-// })
-// "nodeas": "npx nodemon index.js"npx 
+app.listen(port, () => {
+    console.log(`ene port deer server  aslaa http://localhost:${port}`);
+  });
 
-
-const arr = [
-  { name: "zula", age: "30", lastName: "hel", id: 1 },
-];
+const arr = [{ name: "zula", age: "30", lastName: "hi", id: 1 }, { name: "gal", age: "20", lastName: "hello", id: 2 }];
 
 app.get("/", (req, res) => {
-    res.send(arr);
+  res.send(arr);
 });
 
 app.post("/", (req, res) => {
-    res.send(arr.push({name:"duluu", id:2}));
+  console.log("hello world workin over here");
+  arr.push({ name: "duluu", age: "20", lastName: "hello", id: 3 });
+  res.send(arr);
 });
 
-// app.delete("/", (req,res) => {
-//     res.send(arr.push(arr.filter((el) => el.id!==1)));
-// })
-
-app.delete("/", (req,res) => {
-
-    for (i=0; i<arr.length; i++){
-        if(arr[i] == 1) {
-            arr.splice(i,1)
-        }
+app.delete("/", (req, res) => {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] == 1) {
+      arr.splice(i, 1);
     }
-    res.send(arr)
-})
+    return arr;
+  }
+  res.send(arr);
+});
+
+
+// app.delete("/", (req, res) => {
+//   // Find the index of the object with id equal to  1
+//   const indexToDelete = arr.findIndex(item => item.id ===  1);
+
+//   // If the object is found, remove it from the array
+//   if (indexToDelete !== -1) {
+//     arr.splice(indexToDelete,  1);
+//   }
+
+//   // Send the updated array as a response
+//   res.send(arr);
+// });
